fa7f97578e5b143199a8d9f510a263d655aff5f9
Ruslan <ruslan.grudistov@gmail.com>
2023-04-06 17:48:06 +0300

    1

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ var lightbox = new SimpleLightbox('.gallery-list a', {
   captionPosition: 'bottom',
   captionDelay: 250,
   scrollZoom: false,
-  download: string,
+  download: 'string',
 });
 
 const unsplashAPI = new UnsplashAPI();
